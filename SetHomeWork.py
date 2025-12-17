dc64c4c209129ef7b5777e980300f99603d5f3af
saiprasad2610 <saiprasad26102004@gmail.com>
2025-12-17 22:11:01 +0530

    HomeWork On Sets

---
+++ SetHomeWork.py
@@ -0,0 +1,23 @@
+s1 = {10,"sai", 20,30}
+s2 = {"kiran Academy",50,30,"sai","Pune"}
+print("This is Set 1:",s1)
+print("This is Set 2:",s2)
+print("This is Intersection of Sets",s1.intersection(s2))  
+print("This is Union of Sets",s1.union(s2))
+
+
+print("-----------Remove the Duplicates from the List-------------")
+l1 = [10,20,10,30,10,40,20,50]
+print("Original List:",l1)
+s = set(l1)
+l2 = list(s)
+print("Final Sorted List: ",l2)
+
+set1={}
+for value in l1:
+    if value in set1:
+        set1[value] +=1
+    else:
+        set1[value] =1 
+for value in l2:
+    print("delete",value,"=",set1[value]-1,"time")
